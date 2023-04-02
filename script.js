9b3db37207a395e7f68ad41e037fb22f7580716c
Leila Sadr <leilasadr54@gmail.com>
2023-04-01 20:53:43 -0700

    Wrote out the first part of the handleInput function

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 $(document).ready (onReady);
 
 function onReady () {
-    
-    console.log("Let's begin!");
+    console.log('Welcome to our calculator!');
+    $('.submit-button').on('click', handleInput)
 }
 
+function handleInput (event) {
+    console.log('We rollin?');
+    event.preventDefault();
+
+    const firstNameInput = $('#firstName-input');
+    const lastNameInput = $('#lastName-input');
+    const IdInput = $('#ID-input');
+    const titleInput = $('#title-input');
+    const annualSalaryInput = $('#anSal-input');
+
+    const firstName = firstNameInput.val();
+    const lastName = lastNameInput.val();
+    const id = IdInput.val();
+    const title = titleInput.val();
+    const annualSalary = annualSalaryInput.val();
+
+    firstNameInput.val('');
+    lastNameInput.val('');
+    IdInput.val('');
+    titleInput.val('');
+    annualSalaryInput.val('');
+
+    $('#employee-data').append(`
+      <tr>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${id}</td>
+        <td>${title}</td>
+        <td>${annualSalary}</td>
+        <td>
+            <button class="delete-btn">❌</button>
+        </td>
+      </tr>`)
+
+}
